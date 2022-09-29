e32f3361094e7943c1ae3fdcd21a541205c06ecb
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-09-29 16:23:11 -0500

    add routes to bowling

---
+++ My project/Assets/BowlingLaneRoutes.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingLaneRoutes : MonoBehaviour
+{
+    public List<Transform> LaneToSide;
+    public List<Transform> SideToLane;
+    public List<Transform> LaneToBar;
+}
