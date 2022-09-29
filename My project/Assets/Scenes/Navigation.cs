67f903503e43a65f015d505ae48202e079ac928b
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-09-29 09:52:43 -0500

    Add locations for NPC to go to

--- My project/Assets/Scenes/Navigation.cs
+++ My project/Assets/Scenes/Navigation.cs
@@ -5,8 +5,11 @@ using UnityEngine.AI;
 public class Navigation : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] NavMeshAgent nav;
+    private NavMeshAgent nav;
     [SerializeField] GameObject player;
+    [SerializeField] List<Transform> locations;
+
+    [SerializeField] bool test = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,19 @@ public class Navigation : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        //nav.destination = player.transform.position;
+        if (test)
+        {
+            FacePlayer();
+        }
+        else
+        {
+            nav.destination = locations[1].position;
+        }
+    }
+
+    void FacePlayer()
     {
         nav.destination = player.transform.position;
     }
