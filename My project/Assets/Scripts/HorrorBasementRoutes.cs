bc58dde710750278bcc841523c3bc27e90d9eab2
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-10-14 17:11:31 -0500

    Begin set up of horror basement NPC

--- My project/Assets/Scripts/HorrorBasementRoutes.cs
+++ My project/Assets/Scripts/HorrorBasementRoutes.cs
@@ -4,20 +4,13 @@ using UnityEngine;
 
 public class HorrorBasementRoutes : Routes
 {
-    public List<Transform> LaneToSide;
-    public List<Transform> SideToLane;
-    public List<Transform> LaneToBar;
-    public List<Transform> BarToLane;
-    public List<Transform> Bar;
-
+    public List<Transform> Laundry;
+    public List<Transform> BottomOfStairs;
     public override Dictionary<string, List<Transform>> GetRoutes()
     {
         Dictionary<string, List<Transform>> dic = new Dictionary<string, List<Transform>>();
-        dic.Add("LaneToSide", LaneToSide);
-        dic.Add("SideToLane", SideToLane);
-        dic.Add("LaneToBar", LaneToBar);
-        dic.Add("BarToLane", BarToLane);
-        dic.Add("Bar", Bar);
+        dic.Add("Laundry", Laundry);
+        dic.Add("BottomOfStairs", BottomOfStairs);
         return dic;
     }
 }
