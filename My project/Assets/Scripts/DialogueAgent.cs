d788295c8db07b71a7094eb983b8af798e4f9243
Jeremy Fischer <jeremyfischer52@gmail.com>
2022-10-17 16:15:11 -0500

    fixed indecator staying after dialogue starts

--- My project/Assets/Scripts/DialogueAgent.cs
+++ My project/Assets/Scripts/DialogueAgent.cs
@@ -42,7 +42,7 @@ public class DialogueAgent : MonoBehaviour
         self.transform.LookAt(gm.player.transform);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         //TODO do not allow interaction if NPC is moving.
         if (other.tag == "Player")
