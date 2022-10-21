d6d50a08a32fcceb697cf91edfca2622c623fb3b
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-10-21 11:13:39 -0500

    Add rope wiggle effect

--- My project/Assets/Scenes/pushtest.cs
+++ My project/Assets/Scenes/pushtest.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class pushtest : MonoBehaviour
 {
+    private Rigidbody rb;
+    private bool coolDown = false;
+
     // Start is called before the first frame update
     void Start()
     {
-
-        this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(5, 0, 30)); 
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(5, 0, 50)); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!coolDown)
+        {
+            StartCoroutine(WiggleTick());
+        }
+    }
+
+    private void ApplySmallWiggle()
+    {
         
+        rb.AddForce(new Vector3(Random.Range(0, 5), 0, Random.Range(0, 5)));
+    }
+
+    IEnumerator WiggleTick()
+    {
+        coolDown = true;
+        ApplySmallWiggle();
+        yield return new WaitForSeconds(3);
+        coolDown = false;
     }
 }
