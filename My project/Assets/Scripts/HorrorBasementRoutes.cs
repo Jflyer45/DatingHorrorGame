38c81d693c32ca92c9ea1d562f8cef90f4bd56c4
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-11-12 20:53:33 -0600

    Better AI with random room inspections

--- My project/Assets/Scripts/HorrorBasementRoutes.cs
+++ My project/Assets/Scripts/HorrorBasementRoutes.cs
@@ -12,6 +12,13 @@ public class HorrorBasementRoutes : Routes
     public List<Transform> FarRightCorner;
     public List<Transform> FarLeftCorner;
 
+    public List<Transform> Bathroom;
+    public List<Transform> TableRoom;
+    public List<Transform> TVRoom;
+    public List<Transform> BedRoom;
+    public List<Transform> SpawnRoom;
+    public List<Transform> ExitDoor;
+
     public override Dictionary<string, List<Transform>> GetRoutes()
     {
         Dictionary<string, List<Transform>> dic = new Dictionary<string, List<Transform>>();
@@ -22,6 +29,13 @@ public class HorrorBasementRoutes : Routes
         dic.Add("CloseLeftCorner", CloseLeftCorner);
         dic.Add("FarRightCorner", FarRightCorner);
         dic.Add("FarLeftCorner", FarLeftCorner);
+
+        dic.Add("Bathroom", Bathroom);
+        dic.Add("TableRoom", TableRoom);
+        dic.Add("TVRoom", TVRoom);
+        dic.Add("BedRoom", BedRoom);
+        dic.Add("SpawnRoom", SpawnRoom);
+        dic.Add("ExitDoor", ExitDoor);
         return dic;
     }
 }
